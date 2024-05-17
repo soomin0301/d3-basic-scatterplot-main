e77797b8ce87b116673d01b838fb8e26309a8b7d
soomin0301 <12dltnals@gmail.com>
2024-05-17 14:21:51 +0900

    Update starter.js

--- src/starter.js
+++ src/starter.js
@@ -45,99 +45,234 @@ let xUnit;
 let yUnit;
 let legendRects;
 let legendTexts;
+let asiaSelected = false;
+let chinaSelected = false;
+let usSelected = false;
+let europeSelected = false;
+let africaSelected = false;
 
-d3.csv("data/gapminder_combined.csv").then((raw_data) => {
-  data = raw_data.map((d) => {
-    d.population = parseInt(d.population);
-    d.income = parseInt(d.income);
-    d.year = parseInt(d.year);
-    d.life_expectancy = parseInt(d.life_expectancy);
-    return d;
-  });
-  //   console.log(raw_data);
-
-  region = [...new Set(data.map((d) => d.region))];
-  console.log(region);
-  // xScale.domain(d3.extent(data, (d) => d.income));
-  xScale.domain([500, d3.max(data, (d) => d.income)]);
-  yScale.domain(d3.extent(data, (d) => d.life_expectancy));
-  radiusScale.domain([0, d3.max(data, (d) => d.population)]);
-  colorScale.domain(region);
-
-  svg
-    .append("g")
-    .attr("class", "x-axis")
-    .attr("transform", `translate(0, ${height - margin.bottom})`)
-    .call(xAxis);
-
-  svg
-    .append("g")
-    .attr("class", "y-axis")
-    .attr("transform", `translate(${margin.left}, 0)`)
-    .call(yAxis);
-
-  circles = svg
-    .selectAll("circles")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", (d) => xScale(d.income))
-    .attr("cy", (d) => yScale(d.life_expectancy))
-    .attr("r", (d) => radiusScale(d.population))
-    .attr("fill", (d) => colorScale(d.region))
-    .attr("stroke", "#fff")
-    .on("mousemove", function (event, d, index) {
-      tooltip
-        .style("left", event.pageX + "px")
-        .style("top", event.pageY - 120 + "px")
-        .style("display", "block")
-        .style("background-color", "rgba(255, 255, 255, 0.9")
-        .style("border", "1px solid black")
-        .html(
-          `<div>${d.country} <span class = "test" style= "font-size:12px;"> <br/>Life expectancy: <br/><span style="font-size:20px;">${d.life_expectancy}`
-        );
-
-      d3.select(this).style("stroke-width", 3).attr("stroke", "#111");
-    })
-    .on("mouseout", function () {
-      tooltip.style("display", "none");
-      d3.select(this).style("stroke-width", 1).attr("stroke", "#fff");
+d3.csv("data/gapminder_combined.csv")
+  .then((raw_data) => {
+    data = raw_data.map((d) => {
+      d.population = parseInt(d.population);
+      d.income = parseInt(d.income);
+      d.year = parseInt(d.year);
+      d.life_expectancy = parseInt(d.life_expectancy);
+      return d;
     });
-  // Units
-  xUnit = svg
-    .append("text")
-    .attr("transform", `translate(${width / 2}, ${height - 10})`)
-    .text("GDP per capita")
-    .attr("class", "unit");
+    //   console.log(raw_data);
 
-  yUnit = svg
-    .append("text")
-    .attr("transform", "translate(20," + height / 2 + ") rotate(-90)")
-    .text("Life expectancy")
-    .attr("class", "unit");
+    region = [...new Set(data.map((d) => d.region))];
+    console.log(region);
+    // xScale.domain(d3.extent(data, (d) => d.income));
+    xScale.domain([500, d3.max(data, (d) => d.income)]);
+    yScale.domain(d3.extent(data, (d) => d.life_expectancy));
+    radiusScale.domain([0, d3.max(data, (d) => d.population)]);
+    colorScale.domain(region);
 
-  // Legend
-  legendRects = svg
-    .selectAll("legend-rects")
-    .data(region)
-    .enter()
-    .append("rect")
-    .attr("x", (d, i) => width - margin.right - 83)
-    .attr("y", (d, i) => height - margin.bottom - 70 - 25 * i)
-    .attr("width", 12)
-    .attr("height", 17)
-    .attr("fill", (d) => colorScale(d));
+    svg
+      .append("g")
+      .attr("class", "x-axis")
+      .attr("transform", `translate(0, ${height - margin.bottom})`)
+      .call(xAxis);
 
-  legendTexts = svg
-    .selectAll("legend-texts")
-    .data(region)
-    .enter()
-    .append("text")
-    .attr("x", (d, i) => width - margin.right - 83 + 20)
-    .attr("y", (d, i) => height - margin.bottom - 70 - 25 * i + 15)
-    .text((d) => d)
-    .attr("class", "legend-texts");
-});
+    svg
+      .append("g")
+      .attr("class", "y-axis")
+      .attr("transform", `translate(${margin.left}, 0)`)
+      .call(yAxis);
+
+    circles = svg
+      .selectAll("circles")
+      .data(data)
+      .enter()
+      .append("circle")
+      .attr("cx", (d) => xScale(d.income))
+      .attr("cy", (d) => yScale(d.life_expectancy))
+      .attr("r", (d) => radiusScale(d.population))
+      .attr("fill", (d) => colorScale(d.region))
+      .attr("stroke", "#fff");
+    // .on("mousemove", function (event, d, index) {
+    //   tooltip
+    //     .style("left", event.pageX + "px")
+    //     .style("top", event.pageY - 120 + "px")
+    //     .style("display", "block")
+    //     .style("background-color", "rgba(255, 255, 255, 0.9")
+    //     .style("border", "1px solid black")
+    //     .html(
+    //       `<div>${d.country} <span class = "test" style= "font-size:12px;"> <br/>Life expectancy: <br/><span style="font-size:20px;">${d.life_expectancy}`
+    //     );
+
+    //   d3.select(this).style("stroke-width", 3).attr("stroke", "#111");
+    // })
+    // .on("mouseout", function () {
+    //   tooltip.style("display", "none");
+    //   d3.select(this).style("stroke-width", 1).attr("stroke", "#fff");
+    // });
+
+    // Units
+    xUnit = svg
+      .append("text")
+      .attr("transform", `translate(${width / 2}, ${height - 10})`)
+      .text("GDP per capita")
+      .attr("class", "unit");
+
+    yUnit = svg
+      .append("text")
+      .attr("transform", "translate(20," + height / 2 + ") rotate(-90)")
+      .text("Life expectancy")
+      .attr("class", "unit");
+
+    // Legend
+    legendRects = svg
+      .selectAll("legend-rects")
+      .data(region)
+      .enter()
+      .append("rect")
+      .attr("x", (d, i) => width - margin.right - 83)
+      .attr("y", (d, i) => height - margin.bottom - 70 - 25 * i)
+      .attr("width", 12)
+      .attr("height", 17)
+      .attr("fill", (d) => colorScale(d));
+
+    legendTexts = svg
+      .selectAll("legend-texts")
+      .data(region)
+      .enter()
+      .append("text")
+      .attr("x", (d, i) => width - margin.right - 83 + 20)
+      .attr("y", (d, i) => height - margin.bottom - 70 - 25 * i + 15)
+      .text((d) => d)
+      .attr("class", "legend-texts");
+
+    //Button Asia
+    d3.select("#button-asia").on("click", () => {
+      asiaSelected = !asiaSelected;
+      chinaSelected = false;
+      usSelected = false;
+      europeSelected = false;
+      africaSelected = false;
+
+      d3.select("#text-desc").text("Asia Selected");
+      d3.select("#button-asia").classed("button-clicked", asiaSelected);
+      d3.select("#button-china").classed("button-clicked", false);
+      d3.select("#button-us").classed("button-clicked", false);
+      d3.select("#button-europe").classed("button-clicked", false);
+      d3.select("#button-africa").classed("button-clicked", false);
+
+      circles.attr("fill", (d) => {
+        if (asiaSelected) {
+          return d.region == "asia" ? colorScale(d.region) : "rgba(0,0,0,0.1)";
+        } else {
+          return colorScale(d.region);
+        }
+      });
+    });
+    // Button China
+    d3.select("#button-china").on("click", () => {
+      chinaSelected = !chinaSelected;
+      asiaSelected = false;
+      usSelected = false;
+      europeSelected = false;
+      africaSelected = false;
+
+      d3.select("#text-desc").text("China Selected");
+      d3.select("#button-china").classed("button-clicked", chinaSelected);
+      d3.select("#button-asia").classed("button-clicked", false);
+      d3.select("#button-us").classed("button-clicked", false);
+      d3.select("#button-europe").classed("button-clicked", false);
+      d3.select("#button-africa").classed("button-clicked", false);
+
+      circles.attr("fill", (d) => {
+        if (chinaSelected) {
+          return d.country == "China"
+            ? colorScale(d.region)
+            : "rgba(0,0,0,0.1)";
+        } else {
+          return colorScale(d.region);
+        }
+      });
+    });
+
+    // Button US
+    d3.select("#button-us").on("click", () => {
+      usSelected = !usSelected;
+      asiaSelected = false;
+      chinaSelected = false;
+      europeSelected = false;
+      africaSelected = false;
+
+      d3.select("#text-desc").text("US Selected");
+      d3.select("#button-us").classed("button-clicked", usSelected);
+      d3.select("#button-asia").classed("button-clicked", false);
+      d3.select("#button-china").classed("button-clicked", false);
+      d3.select("#button-europe").classed("button-clicked", false);
+      d3.select("#button-africa").classed("button-clicked", false);
+
+      circles.attr("fill", (d) => {
+        if (usSelected) {
+          return d.country == "United States"
+            ? colorScale(d.region)
+            : "rgba(0,0,0,0.1)";
+        } else {
+          return colorScale(d.region);
+        }
+      });
+    });
+    // Button Europe
+    d3.select("#button-europe").on("click", () => {
+      europeSelected = !europeSelected;
+      asiaSelected = false;
+      chinaSelected = false;
+      usSelected = false;
+      africaSelected = false;
+
+      d3.select("#text-desc").text("Europe Selected");
+      d3.select("#button-europe").classed("button-clicked", europeSelected);
+      d3.select("#button-asia").classed("button-clicked", false);
+      d3.select("#button-china").classed("button-clicked", false);
+      d3.select("#button-us").classed("button-clicked", false);
+      d3.select("#button-africa").classed("button-clicked", false);
+
+      circles.attr("fill", (d) => {
+        if (europeSelected) {
+          return d.region == "europe"
+            ? colorScale(d.region)
+            : "rgba(0,0,0,0.1)";
+        } else {
+          return colorScale(d.region);
+        }
+      });
+    });
+    // Button Africa
+    d3.select("#button-africa").on("click", () => {
+      africaSelected = !africaSelected;
+      asiaSelected = false;
+      chinaSelected = false;
+      usSelected = false;
+      europeSelected = false;
+
+      d3.select("#text-desc").text("Africa Selected");
+      d3.select("#button-africa").classed("button-clicked", africaSelected);
+      d3.select("#button-asia").classed("button-clicked", false);
+      d3.select("#button-china").classed("button-clicked", false);
+      d3.select("#button-us").classed("button-clicked", false);
+      d3.select("#button-europe").classed("button-clicked", false);
+
+      circles.attr("fill", (d) => {
+        if (africaSelected) {
+          return d.region == "africa"
+            ? colorScale(d.region)
+            : "rgba(0,0,0,0.1)";
+        } else {
+          return colorScale(d.region);
+        }
+      });
+    });
+  })
+  .catch((error) => {
+    console.error("Error loading CSV data: ", error);
+  });
 
 ////////////////////////////  Resize  //////////////////////////////
 window.addEventListener("resize", () => {
